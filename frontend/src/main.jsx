a2456727e832a341d14e1165366427aa2413275a
Sirimalla Bharathwin <bharathwinsirimalla@gmail.com>
2026-05-13 20:46:10 +0530

    change url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,20 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx' 
+import App from './App.jsx'
+import axios from 'axios'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './redux/store.js'
-import {Provider} from "react-redux"
+import { Provider } from 'react-redux'
+
+const backendURL = import.meta.env.VITE_BACKEND_URL
+if (backendURL) {
+  axios.defaults.baseURL = backendURL
+}
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 )
